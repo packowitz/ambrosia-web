cd7b2295bf2180be1b7f96c31b1c544c2fd67cb1
packowitz <packowitz@gmail.com>
2020-04-29 15:00:22 +0200

    icons on hero base overview

--- src/app/herobase/herobase-list.page.ts
+++ src/app/herobase/herobase-list.page.ts
@@ -5,15 +5,18 @@ import {ToastController} from '@ionic/angular';
 import {HeroBase} from '../domain/herobase.model';
 import {ConverterService} from '../services/converter.service';
 import {Model} from '../services/model.service';
+import {Router} from '@angular/router';
 
 
 export class HeroClassRarity {
     heroClass: string;
     rarity: string;
+    stars: number;
 
-    constructor(heroClass: string, rarity: string) {
+    constructor(heroClass: string, rarity: string, stars: number) {
         this.heroClass = heroClass;
         this.rarity = rarity;
+        this.stars = stars;
     }
 }
 
@@ -21,41 +24,79 @@ export class HeroClassRarity {
     selector: 'herobase-list',
     templateUrl: 'herobase-list.page.html'
 })
-export class HerobaseListPage implements OnInit {
+export class HerobaseListPage {
 
-    heroBaseList: HeroBase[] = [];
     heroClassList: HeroClassRarity[] = [];
+
+    sorting = 'RarityDesc';
+    onlyCloneable = false;
+    onlyStartingHeroes = false;
+    showSimpleHeroes = true;
+    showCommonHeroes = true;
+    showUncommonHeroes = true;
+    showRareHeroes = true;
+    showEpicHeroes = true;
 
     constructor(public enumService: EnumService,
                 private backendService: BackendService,
                 private toastCtrl: ToastController,
                 private converter: ConverterService,
-                private model: Model) {
+                private model: Model,
+                private router: Router) {
     }
 
-    ngOnInit(): void {
+    ionViewWillEnter(): void {
 
         if (!this.model.baseHeroes) {
             this.backendService.getHeroBases().subscribe(data => {
                 this.model.baseHeroes = data;
-                this.setHeroList(data);
+                this.setHeroList();
             });
         } else {
-            this.setHeroList(this.model.baseHeroes);
+            this.setHeroList();
         }
     }
 
-    setHeroList(heroList: HeroBase[]) {
-        this.heroBaseList = heroList;
-        heroList.forEach(hero => {
-            if (!this.heroClassList.find(c => c.heroClass === hero.heroClass && c.rarity === hero.rarity)) {
-                this.heroClassList.push(new HeroClassRarity(hero.heroClass, hero.rarity));
+    setHeroList() {
+        let classList = [];
+        this.model.baseHeroes.filter(h => {
+            if (this.onlyCloneable && !h.recruitable) { return false; }
+            if (this.onlyStartingHeroes && !h.startingHero) { return false; }
+            if (!this.showSimpleHeroes && h.rarity === 'SIMPLE') { return false; }
+            if (!this.showCommonHeroes && h.rarity === 'COMMON') { return false; }
+            if (!this.showUncommonHeroes && h.rarity === 'UNCOMMON') { return false; }
+            if (!this.showRareHeroes && h.rarity === 'RARE') { return false; }
+            if (!this.showEpicHeroes && h.rarity === 'EPIC') { return false; }
+            return true;
+        }).forEach(hero => {
+            if (!classList.find(c => c.heroClass === hero.heroClass && c.rarity === hero.rarity)) {
+                classList.push(new HeroClassRarity(hero.heroClass, hero.rarity, this.converter.rarityStars(hero.rarity)));
             }
         });
+        this.sort(classList);
+    }
+
+    sort(classList: HeroClassRarity[]) {
+        if (this.sorting === 'RarityAsc') {
+            this.heroClassList = classList.sort((a: HeroClassRarity, b: HeroClassRarity) => a.stars === b.stars ? a.heroClass < b.heroClass ? -1 : 1 : a.stars - b.stars);
+        }
+        if (this.sorting === 'RarityDesc') {
+            this.heroClassList = classList.sort((a: HeroClassRarity, b: HeroClassRarity) => a.stars === b.stars ? a.heroClass < b.heroClass ? -1 : 1 : b.stars - a.stars);
+        }
+        if (this.sorting === 'NameAsc') {
+            this.heroClassList = classList.sort((a: HeroClassRarity, b: HeroClassRarity) => a.heroClass === b.heroClass ? b.stars - a.stars : a.heroClass < b.heroClass ? -1 : 1);
+        }
+        if (this.sorting === 'NameDesc') {
+            this.heroClassList = classList.sort((a: HeroClassRarity, b: HeroClassRarity) => a.heroClass === b.heroClass ? b.stars - a.stars : a.heroClass < b.heroClass ? 1 : -1);
+        }
     }
 
     getHero(heroClass: HeroClassRarity, color: string): HeroBase {
-        return this.heroBaseList.find(hero =>
+        return this.model.baseHeroes.find(hero =>
             hero.heroClass === heroClass.heroClass && hero.rarity === heroClass.rarity && hero.color === color);
+    }
+
+    gotoHero(hero: HeroBase) {
+        this.router.navigateByUrl('/herobase/edit/' + hero.id);
     }
 }
