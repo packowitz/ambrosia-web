c7b13094f3893ecac9f58813dda1d187a86a68f5
packowitz <packowitz@gmail.com>
2020-06-24 13:08:26 +0200

    logging

--- src/app/services/model.service.ts
+++ src/app/services/model.service.ts
@@ -320,6 +320,11 @@ export class Model {
         }
         if (data.currentMap) {
             this.updatePlayerMap(data.currentMap);
+            if (!this.currentMap) {
+                console.log("currentMap not set");
+                console.log(data.currentMap);
+                console.log(this.progress);
+            }
         }
         if (data.ongoingBattle) {
             if (data.ongoingBattle.status === 'WON' || data.ongoingBattle.status === 'LOST') {
